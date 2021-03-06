1c9faf6f6b7acc71c68d35dc08cda9f11812c00b
SamGarner <samuel.w.garner@gmail.com>
2021-03-05 19:25:23 -0600

    Feat: Add buttons to resize drawing grid.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const sketchPad = document.querySelector(".sketch-pad");
 const resetButton = document.querySelector('.reset-button');
+const sizeButtons = document.querySelectorAll('.size-button');
+var gridSize = 20;
 
 let generateGrid = gridWidth => { // not function
   let gridArea = gridWidth * gridWidth;
@@ -40,5 +42,30 @@ function colorCell() {  // DOES NOT WORK WITH ARROW NOTATION: undefined 'this'
   this.style.borderStyle = 'none';
 }
 
-generateGrid(30);
+function clearGrid() {
+  let gridArray = Array.from(sketchPad.childNodes); //.forEach((node) => node.remove()); IE support forEach nodelist
+  gridArray.forEach(node => node.remove());
+}
+
+let resizeGrid = (event) => {  // can be => function
+  switch(event.target.dataset.size)
+  {
+    case '20': // must be strings
+      gridSize = 20;
+      break;
+    case '30':
+      gridSize = 30;
+      break;
+    case '50':
+      gridSize = 50;
+      break;
+  };
+  clearGrid();
+  generateGrid(gridSize);
+};
+// event.target.dataset.size
+// gridSize
+
+generateGrid(gridSize);
 resetButton.addEventListener('click', resetGrid); // no () on resetGrid
+sizeButtons.forEach(sizeButton => sizeButton.addEventListener('click', resizeGrid));// resizeGrid))
